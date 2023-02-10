1f2eadf44f571e4b69fc7658df2ea7f1fb258e48
axlynaylyngarcia <axlyngarcia0201@gmail.com>
2023-02-10 07:46:00 -0500

    se creó el diagrama de flujo y se agregó al README

---
+++ conversor_temperatura.py
@@ -0,0 +1,19 @@
+print("........................................")
+print("..... VALOR EN GRADOS CENTIGRADOS ......")
+print("........................................")
+
+#input
+k = int(input(" digite grados kelvin: "))
+f = int(input(" digite grados farenheit: "))
+
+#processing
+f 
+k 
+
+#output
+print("...............................")
+print("......... RESULTADOS ..........")
+print("...............................")
+
+print("La suma de " + str(x) + " + " + str(y) + " es " + str(s))
+print("La resta de " + str(x) + " - " + str(y) + " es " + str(r))
